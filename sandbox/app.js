87f82bbb88ebe6a43e7e92aa660e42bbcbe7265a
Y Paing <helloye@Ys-iMac.fios-router.home>
2019-04-07 01:36:16 -0400

    Re shuffled some files around. Testing out expressjs! :D

---
+++ sandbox/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => {
+  console.log('Request:', req.ip);
+  res.send('Hello World!')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
